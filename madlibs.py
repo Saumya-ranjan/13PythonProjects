546f39debbcea19ceecb0929afb3006d7d63c318
Saumyaranjan Parida <57477827+Saumya-ranjan@users.noreply.github.com>
2021-12-23 20:29:25 +0530

    Concatenating Project

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+#concatenating the string is the main purpose of this madlibs.py
+
+
+youtuber = "help"
+
+print("hello there "+ youtuber)
+print("hello there {}".format(youtuber))
+print(f"hello there {youtuber}")
